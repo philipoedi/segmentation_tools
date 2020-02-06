bd34237f5adc6ef3e1622726f92d45c0a8cd2128
OediP <philip.oedi@umusic.com>
2020-02-06 15:17:34 +0100

    folder structure
    https://towardsdatascience.com/whats-init-for-me-d70a312da583

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  6 15:00:52 2020
+
+@author: OediP
+"""
+
+from setuptools import setup
+
+setup(name='segmentation_tools',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/philipoedi/segmentation_tools',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['segmentation_tools'],
+      zip_safe=False)
