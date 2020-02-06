640394e04c23bade4bc4f7d1a8421dca98366a08
OediP <philip.oedi@umusic.com>
2020-02-06 15:46:57 +0100

    sliding_window , test
    added test cases
    and changed sliding window

---
+++ segmentation_tools/tests/test_top_down.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  6 15:25:05 2020
+
+@author: OediP
+"""
+
+# -*- coding: utf-8 -*-
+import numpy as np
+from scipy.signal import sawtooth
+
+import segmentation_tools
+
+x = np.arange(0,20,0.1)
+sin_curve = np.sin(x)
+saw_curve = sawtooth(x,0.1)
+saw_sin = sin_curve*saw_curve 
+
+max_error = 1
+k = segmentation_tools.top_down()
+k.fit(saw_sin,max_error)
+
+assert k.error > max_error, "error to big"
+assert np.max(k.labels) == 6, "wrong segments"
+print("everything fine")
