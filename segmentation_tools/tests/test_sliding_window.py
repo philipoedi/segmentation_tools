be7d9d16aa6611029b7bd1b870a24058c0c2a216
OediP <philip.oedi@umusic.com>
2020-03-03 15:48:00 +0100

    cosmetics
    lines gekürzt
    multidim noch zu machen

--- segmentation_tools/tests/test_sliding_window.py
+++ segmentation_tools/tests/test_sliding_window.py
@@ -11,7 +11,7 @@ saw_sin = sin_curve*saw_curve
 
 max_error = 0.7
 k = segmentation_tools.sliding_window()
-k.fit(saw_sin.reshape(len(saw_sin),1),max_error,"linear_regression")
+k.fit(saw_sin.reshape(len(saw_sin),1),max_error,"linear_regression","max")
 
 k.segment_plot()
 # check number of labels and segment borders
