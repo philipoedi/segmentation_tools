8d23ec8f236e084a9a2a1865490bc596f2e3ef3f
OediP <philip.oedi@umusic.com>
2020-03-03 14:33:03 +0100

    test, bup
    bottom up segmentsboders
    new tests

--- segmentation_tools/tests/test_top_down.py
+++ segmentation_tools/tests/test_top_down.py
@@ -9,7 +9,6 @@ Created on Thu Feb  6 15:25:05 2020
 import numpy as np
 from scipy.signal import sawtooth
 import matplotlib.pyplot as plt
-
 import segmentation_tools
 
 x = np.arange(0,20,0.1)
@@ -21,14 +20,28 @@ saw_sin = sin_curve*saw_curve
 max_error = 0.5
 k = segmentation_tools.top_down()
 k.fit(saw_sin.reshape(len(saw_sin),1),max_error,"linear_regression")
-print(k.segment_borders)
-k.segment_plot()
 
-assert k.error > max_error, "error to big"
-assert np.max(k.labels) == 6, "wrong segments"
+k.segment_plot()
+# check number of labels and segment borders
+assert np.max(k.labels)+1 == len(k.segment_borders),"num labels don't match"
+# check max_error
+assert np.max([k.calculate_error(k.segments[i].data) for i in range(len(k.segments))]) < max_error,"error to big"
 print("everything fine")
 
 
+k = segmentation_tools.top_down()
+k.fit(saw_sin.reshape(len(saw_sin),1),max_error,"linear_interpolation")
+
+k.segment_plot()
+# check number of labels and segment borders
+assert np.max(k.labels)+1 == len(k.segment_borders),"num labels don't match"
+# check max_error
+assert np.max([k.calculate_error(k.segments[i].data) for i in range(len(k.segments))]) < max_error,"error to big"
+print("everything fine")
+
+
+
+#step function
 step = np.zeros(len(x))
 size = int(len(step) / 4)
 step[:size] = step[:size]+1
@@ -36,21 +49,23 @@ step[2*size:3*size] = step[2*size:3*size]+1
 plt.plot(step)
 
 
-#step function
 k = segmentation_tools.top_down()
 
 print("check step function segmentation with linear regression")
 k.fit(step.reshape(len(saw_sin),1),max_error,"linear_regression")
-assert k.error < max_error, "error to big"
-assert np.max(k.labels) == 3, "wrong segments"
+k.segment_plot()
+# check number of labels and segment borders
+assert np.max(k.labels)+1 == len(k.segment_borders),"num labels don't match"
+# check max_error
+assert np.max([k.calculate_error(k.segments[i].data) for i in range(len(k.segments))]) < max_error,"error to big"
 print("everything fine")
 
 print("check step function segmentation with linear interpolation")
+k = segmentation_tools.top_down()
 k.fit(step.reshape(len(saw_sin),1),max_error,"linear_interpolation")
-assert k.error < max_error, "error to big"
-assert np.max(k.labels) == 3, "wrong segments"
+k.segment_plot()
+# check number of labels and segment borders
+assert np.max(k.labels)+1 == len(k.segment_borders),"num labels don't match"
+# check max_error
+assert np.max([k.calculate_error(k.segments[i].data) for i in range(len(k.segments))]) < max_error,"error to big"
 print("everything fine")
-
-plt.plot(k.labels)
-plt.plot(step)
-plt.show()
